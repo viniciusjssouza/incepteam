d936d1c2f2dd5142d485bf76c5b480b9c8086be8
Vinicius Souza <viniciusjssouza@gmail.com>
2018-05-05 16:16:03 -0300

    create hill climbing algorithm

---
+++ algorithm/hill_climbing.py
@@ -0,0 +1,14 @@
+
+class HillClimbing:
+
+    DEFAULT_N_ITERATIONS = 10000
+
+    def __init__(self, people, teams, n_iterations=DEFAULT_N_ITERATIONS):
+        self.people = people
+        self.teams = teams
+        self.n_iterations = n_iterations
+
+
+    def search(self, iterations = 10):
+        pass
+
