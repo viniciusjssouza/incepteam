deda9cf83f661c2240b5fec173b7cbd45391468e
madjomba <matheusbpaula@gmail.com>
2018-05-05 18:58:56 -0300

    imporoves team_allocation ==

--- model/team_allocation.py
+++ model/team_allocation.py
@@ -3,13 +3,13 @@ class TeamAllocation:
 
     def __init__(self, team_name, members):
         self.team_name = team_name
-        self.members = members
+        self.members = members.sort
 
     def __hash__(self):
         return self.team_name
 
     def __eq__(self, other):
-        return self.team_name == other.team_name
+        return self.team_name == other.team_name and self.members == other.members
 
     def __str__(self):
         return "Team: %s   Members: %s" % (self.team_name, self.members)
